f96b2afdb3938daf400515ee4f8867b54bc6b791
truongdeptrai <34234909+truongkutetk97@users.noreply.github.com>
2019-03-11 10:42:30 +0700

    m

--- stm88Wpf/stm88Wpf/MainWindow.xaml.cs
+++ stm88Wpf/stm88Wpf/MainWindow.xaml.cs
@@ -1,33 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
 
-namespace stm88Wpf
+namespace Wpf.CartesianChart.BasicLine
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class BasicLineExample : UserControl
     {
-        public MainWindow()
+        public BasicLineExample()
         {
             InitializeComponent();
+           
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {   
+                    Title = "Channel 1",
+                    Values = new ChartValues<double> { 0}
+                },
+                new LineSeries
+                {
+                    Title = "Channel 2",
+                    Values = new ChartValues<double> { 0}
+                }
+            };
+
+            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+            YFormatter = value => value.ToString("C");
+
+            //modifying the series collection will animate and update the chart
+            SeriesCollection.Add(new LineSeries
+            {
+                Values = new ChartValues<double> { 0 },
+                LineSmoothness = 0 //straight lines, 1 really smooth lines
+            });
+
+            //modifying any series values will also animate and update the chart
+            SeriesCollection[2].Values.Add(5d);
+
+            DataContext = this;
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+
+        private void startbtn_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            MessageBox.Show("truongdeptrai");
+
+            Random r1 = new Random();
+            SeriesCollection[0].Values.Add(Convert.ToDouble(r1.Next(1,10)));
+            Random r2 = new Random();
+            SeriesCollection[1].Values.Add(Convert.ToDouble(r2.Next(1, 15)));
+            Random r3 = new Random();
+            SeriesCollection[2].Values.Add(Convert.ToDouble(r3.Next(1, 5)));
+
         }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+      
     }
 }
